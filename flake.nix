5d3877975ce3dac831909c51c28e74129635eab5
Andreas Zweili <andreas@zweili.ch>
2024-11-18 19:31:16 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs =
+    { ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.gradle
+          pkgs.jdk23
+        ];
+        NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc
+          pkgs.openssl
+        ];
+        NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+        # ^--- when using direnv, this line will require the 'use flake --impure' option.
+      };
+    };
+}
