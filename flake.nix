bfd9be7ab604a6384749dc9ed9eba9dc5036c3c7
Andreas Zweili <andreas@zweili.ch>
2025-01-26 10:46:15 +0100

    Add example Lox implementation
    It requires Dart 2 to build at the moment.
    https://github.com/munificent/craftinginterpreters/issues/1122

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    dart2-nixpkgs.url = "github:nixos/nixpkgs?rev=27ae052ce0871efbe6bb0b2d5c60cde313a96ff7";
   };
   outputs =
     { ... }@inputs:
     let
-      system = "x86_64-linux";
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      dart2pkgs = inputs.dart2-nixpkgs.legacyPackages.${system};
       java = pkgs.jdk23;
       jdt = (pkgs.jdt-language-server.override { jdk = java; });
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      system = "x86_64-linux";
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
+          dart2pkgs.dart # Dart version 2 is required to build the example Lox
           pkgs.entr
           pkgs.gcc14
           pkgs.gnumake
