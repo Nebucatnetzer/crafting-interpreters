4a9959e8aece116df518d63bce50a256f795d5f4
Andreas Zweili <andreas@zweili.ch>
2025-02-03 14:16:38 +0100

    Add identifiers

--- java/src/main/java/org/zweili/Scanner.java
+++ java/src/main/java/org/zweili/Scanner.java
@@ -1,11 +1,35 @@
 package org.zweili;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", TokenType.AND);
+    keywords.put("class", TokenType.CLASS);
+    keywords.put("else", TokenType.ELSE);
+    keywords.put("false", TokenType.FALSE);
+    keywords.put("for", TokenType.FOR);
+    keywords.put("fun", TokenType.FUN);
+    keywords.put("if", TokenType.IF);
+    keywords.put("nil", TokenType.NIL);
+    keywords.put("or", TokenType.OR);
+    keywords.put("print", TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super", TokenType.SUPER);
+    keywords.put("this", TokenType.THIS);
+    keywords.put("true", TokenType.TRUE);
+    keywords.put("var", TokenType.VAR);
+    keywords.put("while", TokenType.WHILE);
+  }
+
   private int start = 0;
   private int current = 0;
   private int line = 1;
@@ -95,11 +119,22 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = this.source.substring(this.start, this.current);
+    TokenType type = Scanner.keywords.get(text);
+    if (type == null) type = TokenType.IDENTIFIER;
+    addToken(type);
   }
 
   private void number() {
@@ -149,6 +184,14 @@ class Scanner {
   private char peekNext() {
     if (this.current + 1 > +this.source.length()) return '\0';
     return this.source.charAt(this.current + 1);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c < +'z') || (c >= 'A' && c < +'Z') || c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private boolean isDigit(char c) {
