42e6e1a5e740ca8e622735388a1283683da12424
Andreas Zweili <andreas@zweili.ch>
2025-02-03 14:21:03 +0100

    Remove wrong import

--- java/src/main/java/org/zweili/Lox.java
+++ java/src/main/java/org/zweili/Lox.java
@@ -9,7 +9,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.List;
 
 public class Lox {
