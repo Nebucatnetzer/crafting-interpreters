3fc8bc2d75616899d60d10d4be9599abe77d0e4c
Andreas Zweili <andreas@zweili.ch>
2025-02-03 14:51:59 +0100

    Make the lox REPL a package

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     dart2-nixpkgs.url = "github:nixos/nixpkgs?rev=27ae052ce0871efbe6bb0b2d5c60cde313a96ff7";
   };
   outputs =
-    { ... }@inputs:
+    { self, ... }@inputs:
     let
       dart2pkgs = inputs.dart2-nixpkgs.legacyPackages.${system};
       gradle = pkgs.gradle.override {
@@ -14,8 +14,19 @@
       jdt = (pkgs.jdt-language-server.override { jdk = java; });
       pkgs = inputs.nixpkgs.legacyPackages.${system};
       system = "x86_64-linux";
+      loxRepl = pkgs.writeShellApplication {
+        name = "loxr";
+        runtimeInputs = [ java ];
+        text = ''
+          java ${self}/java/src/main/java/org/zweili/Lox.java "$@"
+        '';
+      };
     in
     {
+
+      packages.${system} = {
+        lox-repl = loxRepl;
+      };
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
           dart2pkgs.dart # Dart version 2 is required to build the example Lox
