e1439e30fe703f753c67ccce1919c5f2b5650e1e
Andreas Zweili <andreas@zweili.ch>
2025-04-09 22:06:01 +0200

    Formatting

--- java/org/zweili/lox/Interpreter.java
+++ java/org/zweili/lox/Interpreter.java
@@ -91,7 +91,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     stmt.accept(this);
   }
 
-  void executeBlock(List<Stmt> statements, Environment environment){
+  void executeBlock(List<Stmt> statements, Environment environment) {
     Environment previous = this.environment;
     try {
       this.environment = environment;
