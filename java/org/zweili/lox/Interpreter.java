95d9645c94255381e1cc9ae620e3c2f41639cffa
Andreas Zweili <andreas@zweili.ch>
2025-04-09 20:20:47 +0200

    Implement the Environment

--- java/org/zweili/lox/Interpreter.java
+++ java/org/zweili/lox/Interpreter.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
+  private Environment environment = new Environment();
+
   private boolean isTruthy(Object object) {
     if (object == null) {
       return false;
@@ -72,6 +74,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
+  }
+
+  @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
   }
@@ -94,6 +101,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   public Void visitPrintStmt(Stmt.Print stmt) {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+    environment.define(stmt.name.lexeme, value);
     return null;
   }
 
