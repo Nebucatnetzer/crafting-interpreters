265e23647189fb86ca58cfebb37006bdbac32031
Andreas Zweili <andreas@zweili.ch>
2025-04-09 20:46:29 +0200

    Add declaration

--- java/org/zweili/lox/Parser.java
+++ java/org/zweili/lox/Parser.java
@@ -16,13 +16,25 @@ class Parser {
   List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
     return statements;
   }
 
   private Expr expression() {
     return equality();
+  }
+
+  private Stmt declaration() {
+    try {
+      if (match(TokenType.VAR)) {
+        return varDeclaration();
+      }
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
   }
 
   private Stmt statement() {
@@ -36,6 +48,16 @@ class Parser {
     Expr value = expression();
     consume(TokenType.SEMICOLON, "Expect ';' after value.");
     return new Stmt.Print(value);
+  }
+
+  private Stmt varDeclaration() {
+    Token name = consume(TokenType.IDENTIFIER, "Expected variable name.");
+    Expr initializer = null;
+    if (match(TokenType.EQUAL)) {
+      initializer = expression();
+    }
+    consume(TokenType.SEMICOLON, "Expected ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
   }
 
   private Stmt expressionStatement() {
@@ -110,6 +132,9 @@ class Parser {
     }
     if (match(TokenType.NUMBER, TokenType.STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+    if (match(TokenType.IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
     if (match(TokenType.LEFT_PAREN)) {
       Expr expr = expression();
