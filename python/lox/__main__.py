6ac4e95896adccb7ba0d5ce51e9dbaeb94449069
Andreas Zweili <andreas@zweili.ch>
2025-04-24 19:34:29 +0200

    Add error and report function

--- python/lox/__main__.py
+++ python/lox/__main__.py
@@ -3,23 +3,39 @@ import sys
 from argparse import Namespace
 from pathlib import Path
 
+HAD_ERROR = False
+
 
 def run(line: str) -> None:
     print(line)
+
+
+def error(line: int, message: str) -> None:
+    report(line, "", message)
+
+
+def report(line: int, where: str, message: str) -> None:
+    global HAD_ERROR  # noqa: PLW0603
+    print("[line " + str(line) + "] Error" + where + ": " + message)
+    HAD_ERROR = True
 
 
 def run_file(path: Path) -> None:
     with Path.open(path, "r") as file:
         for line in file.readlines():
             run(line)
+            if HAD_ERROR:
+                sys.exit(65)
 
 
 def run_prompt() -> None:
+    global HAD_ERROR  # noqa: PLW0603
     while True:
         user_input = input("> ")
         if not user_input:
             break
         run(user_input)
+        HAD_ERROR = False
 
 
 def parse_args(args: list[str]) -> Namespace:
